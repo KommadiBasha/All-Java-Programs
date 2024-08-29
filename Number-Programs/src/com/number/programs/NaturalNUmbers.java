d92ce4b661522920c71bc51a2651b790c989b9a9
KommadiBasha <kommadibasha2017@gmail.com>
2024-08-29 08:58:39 +0530

    number programs commit 1

---
+++ Number-Programs/src/com/number/programs/NaturalNUmbers.java
@@ -0,0 +1,13 @@
+package com.number.programs;
+
+public class NaturalNUmbers {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=20;i++)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
---
+++ Number-Programs/src/com/number/programs/WholeNumbers.java
@@ -0,0 +1,15 @@
+package com.number.programs;
+
+public class WholeNumbers {
+
+	public static void main(String[] args) {
+	int num=0;
+	while(num<=20)
+	{
+		System.out.print(num+" ");
+		num++;
+	}
+
+	}
+
+}
